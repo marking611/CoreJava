2a416165223eae53688ee1a44e3298708dec68b4
makai <178431729@qq.com>
2017-08-02 09:56:21 +0800

    Initial commit

---
+++ src/main/java/com/mak/CoreJavaApplication.java
@@ -0,0 +1,12 @@
+package com.mak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreJavaApplication.class, args);
+	}
+}
