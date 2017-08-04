321258990cf05010a72ba0b18d4a6eba8ea92836
makai <178431729@qq.com>
2017-08-04 11:07:33 +0800

    指定启动main方法，避免多个main启动报错

--- src/main/java/com/mak/CoreJavaApplication.java
+++ src/main/java/com/mak/CoreJavaApplication.java
@@ -2,11 +2,13 @@ package com.mak;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class})
 public class CoreJavaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CoreJavaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CoreJavaApplication.class, args);
+    }
 }
