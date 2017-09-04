bccc74a2b40c09bc08aa678e11129f68d3da37c0
marking611 <178431729@qq.com>
2017-09-05 00:04:14 +0800

    UPD 多线程修复

--- src/main/java/com/mak/socket/UDPSocketClient.java
+++ src/main/java/com/mak/socket/UDPSocketClient.java
@@ -1,19 +1,28 @@
 package com.mak.socket;
 
 import java.io.IOException;
-import java.net.*;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 /**
  * Created by makai on 2017/9/4.
  */
 public class UDPSocketClient {
     public static void main(String[] args) throws IOException {
-        byte[] bytes = "UDP客户端001".getBytes();
+        //发送请求
+        byte[] bytes = "UDP客户端002".getBytes();
         InetAddress address = InetAddress.getByName("localhost");
-        DatagramPacket packet = new DatagramPacket(bytes,bytes.length,address,8899);
+        DatagramPacket packet = new DatagramPacket(bytes, bytes.length, address, 8888);
         DatagramSocket socket = new DatagramSocket();
         socket.send(packet); //发送数据包
-        socket.close();
+
+        //接收请求
+        byte[] data2 = new byte[1024];
+        DatagramPacket packet2  = new DatagramPacket(data2,data2.length);
+        socket.receive(packet2); //接收数据包
+        //打印数据包信息
+        System.out.println("我是客户端，服务端说："+new String(packet2.getData()));
 
     }
 }
--- src/main/java/com/mak/socket/UDPSocketServer.java
+++ src/main/java/com/mak/socket/UDPSocketServer.java
@@ -3,26 +3,33 @@ package com.mak.socket;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 
 /**
  * Created by makai on 2017/9/4.
  */
 public class UDPSocketServer {
     public static void main(String[] args) throws IOException {
-        DatagramSocket socket = new DatagramSocket(8899);
-        byte[] bytes = new byte[1024];
-        DatagramPacket packet = new DatagramPacket(bytes,0,bytes.length);
-        while (true){
-            Thread thread = new Thread(){
+        DatagramSocket socket = new DatagramSocket(8888);
+        while (true) {
+            byte[] bytes = new byte[1024];
+            DatagramPacket packet = new DatagramPacket(bytes, 0, bytes.length);
+            socket.receive(packet); //接收数据包 [堵塞]
+            Thread thread = new Thread() {
                 @Override
                 public void run() {
+                    //打印数据包数据
+                    System.out.println("我是服务端，客户端说：" + new String(packet.getData(),0,packet.getLength()));
+                    //向客户端回复信息
+                    InetAddress address = packet.getAddress();
+                    int port = packet.getPort();
+                    byte[] data2 = "连接成功！欢迎您！".getBytes();
+                    DatagramPacket packet2 = new DatagramPacket(data2,0,data2.length,address,port);
                     try {
-                        socket.receive(packet); //接收数据包[阻塞]
-                        System.out.print("我是服务端，客户端说："+new String(bytes,"utf-8"));
+                        socket.send(packet2);
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
-
                 }
             };
             thread.start();
